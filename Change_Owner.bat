9fbaa224cc4a72e27e5d63f97565b598f6c85b5a
rodrigoeasilva <rodrigoeasilva@gmail.com>
2022-12-02 09:46:41 -0300

    Mudar permissionamento
    Esse Script muda o permissionário desde o dono até as permissões de grupo/usuário em pastas/arquivos

---
+++ Change_Owner.bat
@@ -0,0 +1,6 @@
+@CHCP 1252 >NUL
+takeown /f "X:Caminho\Completo do arquivi\" /r
+icacls "X:Caminho\Completo do arquivi\" /t /grant "GG_Nome do grupo":F
+
+takeown /f "X:Caminho\Completo do arquivi\" /r
+icacls "X:Caminho\Completo do arquivi\" /t /grant "GG_Nome do grupo":F
